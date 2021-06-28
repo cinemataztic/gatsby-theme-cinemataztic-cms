876c7af09aa5d9b9449f4c74e1f4b2f38714e2a5
ddennis <dennis@fantastisk.dk>
2021-06-28 11:07:09 +0200

    feat: implement text color and background color on Buttun and external link

--- packages/@cinemataztic/gatsby-theme-cinemataztic-cms/src/components/buttons/Button.js
+++ packages/@cinemataztic/gatsby-theme-cinemataztic-cms/src/components/buttons/Button.js
@@ -1,17 +1,26 @@
 /**
  * Created by @author @ddennis - ddennis.dk aka fantastisk.dk/works aka meresukker.dk on 20-08-2019.
  */
-import React from "react";
-import AniLink from "gatsby-plugin-transition-link/AniLink";
+import React from 'react'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
 
-const Button = ({ to, children, block, type = "primary" }) => {
+const Button = ({ to, children, block, type = 'primary', pageLink }) => {
   // to is the actual pageLink node
-  const asBtnBlock = block ? "btn-block" : "";
-  const path = to && to.slug ? to.slug : "";
+
+  const { btnColor = '#bababa', textColor = '#212529' } = pageLink
+
+  const asBtnBlock = block ? 'btn-block' : ''
+  const path = to && to.slug ? to.slug : ''
   return (
     <AniLink cover to={path} direction="up" duration={0.8} bg="#323232">
-      <button className={`btn  btn-${type} ${asBtnBlock}`}>{children}</button>
+      {/*<button className={`btn  btn-${type} ${asBtnBlock}`}>{children}</button>*/}
+      <button
+        className={`btn py-2 btn-primary ${asBtnBlock}`}
+        style={{ background: btnColor, color: textColor, border: 'none' }}
+      >
+        {children}
+      </button>
     </AniLink>
-  );
-};
-export default Button;
+  )
+}
+export default Button
--- packages/@cinemataztic/gatsby-theme-cinemataztic-cms/src/components/buttons/ExternalLink.js
+++ packages/@cinemataztic/gatsby-theme-cinemataztic-cms/src/components/buttons/ExternalLink.js
@@ -1,20 +1,23 @@
 /**
  * Created by @author @ddennis - ddennis.dk aka fantastisk.dk/works aka meresukker.dk on 17-09-2019.
  */
-import React from "react";
+import React from 'react'
 
-const ExternalLink = ({ to, children }) => {
+const ExternalLink = ({ to, children, pageLink = {} }) => {
+  const { btnColor = '#3a7bad', textColor = '#212529' } = pageLink
+
   return (
     <div>
       <a
         className="btn btn-primary  "
         href={to}
+        style={{ background: btnColor, color: textColor, border: 'none' }}
         target="_blank"
         rel="noopener noreferrer"
       >
-        {children}{" "}
+        {children}{' '}
       </a>
     </div>
-  );
-};
-export default ExternalLink;
+  )
+}
+export default ExternalLink
